4bb321b69f52e62f8530d3bdc8f6cab0059caa88
Stan Strutt <Stan.S@digitaladvantage.org.uk>
2024-10-07 13:39:03 +0100

    add Title

---
+++ src/components/title.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+function Title() {
+    return (
+      <div className="Title">
+        <Helmet>
+          <title>My Portfolio</title>
+        </Helmet>
+      </div>
+    )
+  }
+  
+  export default Title;
