bb3e6a0003b051a01545253779c9f1d1d9d09881
Stan Strutt <Stan.S@digitaladvantage.org.uk>
2024-10-07 13:31:29 +0100

    add Title

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from './pages/home';
 import Portfolio from './pages/portfolio';
 import Navbar from './components/Navbar';
 import "./App.css"
+import Title from './components/title';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <div className="App">
+        <Title />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
