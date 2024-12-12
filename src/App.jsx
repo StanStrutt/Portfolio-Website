e531458b04ae7177f8c225b7930024de647b4f4f
Stan <stan.strutt@gmail.com>
2024-12-12 14:24:23 +0000

    changed background and text colour

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Portfolio from './pages/portfolio';
 import Navbar from './components/Navbar';
-import Title from './components/title';
+// import Title from './components/title';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <div className="App">
-        <Title />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <h2>Welcome to my Portfolio website</h2>
-      <h3>A little bit about myself</h3>
-      <p>My name is stan and im currently an intern at disc college 
-        </p>
-      <p>I am working towards learning and developing my skills in javascript
-         and react as well as a backend developer/software engineer.
-      </p>
-      <p>I have taught myself python, HTML, CSS and am currently looking for opportunities as a software developer.</p>
+    <div className='Home'>
+      <div className='background'>
+        <div className='home-text'>
+          <h1>Home Page</h1>
+          <h2>Welcome to my Portfolio website</h2>
+          <h3>A little bit about myself</h3>
+          <p>My name is stan and im currently an intern at DISC college 
+            </p>
+          <p>I am working towards learning and developing my skills in javascript
+            and react as well as a backend developer/software engineer.
+          </p>
+          <p>I have taught myself python, HTML, CSS and am currently looking for opportunities as a software developer.</p>
+        </div>
+      </div>
     </div>
   );
 };
